647446eb56d91978246d0abc094da7fcce4db76c
Rahul Malhotra <rahulm@paradox.com>
2022-02-26 20:11:56 -0500

    Added complaint endpoint

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ __author__ = "Rahul Malhotra"
 
 from pprint import pprint
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
-from db_operations import client
+from db_operations import client, create_complaint
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Use /all_launches_timeline'
+    return 'Welcome to Snow Clearance Buddy App!!'
 
 
-@app.route('/sample_launch/')
-def sample():
+@app.route('/creator/')
+def creator():
     db = client.Test
     result = db.Creator.find({"Creator": "Rahul Malhotra"})
     result = str(list(result))
     return jsonify(result)
 
 
+@app.route('/complaint/', methods=["GET"])
+def register_complain():
+    person_name = request.args.get('name')
+    address = request.args.get('address')
+    complaint_type = request.args.get('complaint_type')
+    result = create_complaint(person_name, address, complaint_type, threshold=5)
+    return result
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- db_operations.py
+++ db_operations.py
@@ -12,7 +12,12 @@ usrn = os.environ.get("username")
 passw = os.environ.get("password")
 client = MongoClient(
     f"mongodb+srv://{usrn}:{passw}@snowclearancebuddy.7tjmg.mongodb.net/?retryWrites=true&w=majority")
+snow_buddy_db = client.SnowBuddy
 
+
+# db = client.Test
+# resp = db.Creator.find({"Creator": "Rahul Malhotra"})
+# pprint(list(resp))
 
 def check_db_availability(mongo_client: MongoClient) -> None:
     """
@@ -28,6 +33,18 @@ def check_db_availability(mongo_client: MongoClient) -> None:
 
 
 check_db_availability(mongo_client=client)
-db = client.Test
-resp = db.Creator.find({"Creator": "Rahul Malhotra"})
-pprint(list(resp))
+
+
+def find_number_complaints(address, threshold, complaint_type):
+    if complaint_type:
+        compliants = snow_buddy_db.complaints.find({"address": address, "complaint_type": complaint_type})
+    else:
+        compliants = snow_buddy_db.complaints.find({"address": address})
+    number_of_complaints = len(list(compliants))
+    return number_of_complaints
+
+
+def create_complaint(name, address, complaint_type, threshold=5):
+    snow_buddy_db.complaints.insert_one({"name": name, "address": address, "complaint_type": complaint_type})
+    total_complaints = find_number_complaints(address=address, complaint_type=complaint_type, threshold=threshold)
+    return {"Total no. of complaints registered": total_complaints}
