8540abe1abb09133b9b8961c71cf1e0405d622a2
elryan75 <42308657+elryan75@users.noreply.github.com>
2022-02-27 00:36:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ __author__ = "Rahul Malhotra"
 
 from pprint import pprint
 
+import googlemaps
+
 from flask import Flask, jsonify, request
 
-from db_operations import client, create_complaint
+from db_operations import client, create_complaint, add_street, get_new_route
 
 app = Flask(__name__)
 
@@ -30,6 +32,20 @@ def register_complain():
     complaint_type = request.args.get('complaint_type')
     result = create_complaint(person_name, address, complaint_type, threshold=5)
     return result
+
+@app.route('/add/', methods=["GET"])
+def add_new_street():
+    address = request.args.get('address')
+    longitude = request.args.get('longitude')
+    latitude = request.args.get('latitude')
+    date_of_clearing = request.args.get('date_of_clearing')
+    clearing_status = request.args.get('clearing_status')
+    return add_street(address, longitude, latitude, date_of_clearing, clearing_status)
+
+
+@app.route('/compute/')
+def showStreets():
+    return get_new_route()
 
 
 if __name__ == '__main__':
