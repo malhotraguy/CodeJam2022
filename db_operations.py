dcdabb35f830caca404f5dc1c765381fbaa79aac
elryan75 <42308657+elryan75@users.noreply.github.com>
2022-02-27 00:36:40 -0500

    Update db_operations.py

--- db_operations.py
+++ db_operations.py
@@ -2,6 +2,8 @@ __author__ = "Rahul Malhotra"
 
 from pprint import pprint
 
+import googlemaps
+from flask import jsonify
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -10,10 +12,12 @@ from pymongo.errors import ConnectionFailure
 load_dotenv()  # use dotenv to hide sensitive credential as environment variables
 usrn = os.environ.get("username")
 passw = os.environ.get("password")
+key = os.environ.get("key")
 client = MongoClient(
     f"mongodb+srv://{usrn}:{passw}@snowclearancebuddy.7tjmg.mongodb.net/?retryWrites=true&w=majority")
 snow_buddy_db = client.SnowBuddy
 
+gmaps = googlemaps.Client(key=key)
 
 # db = client.Test
 # resp = db.Creator.find({"Creator": "Rahul Malhotra"})
@@ -48,3 +52,36 @@ def create_complaint(name, address, complaint_type, threshold=5):
     snow_buddy_db.complaints.insert_one({"name": name, "address": address, "complaint_type": complaint_type})
     total_complaints = find_number_complaints(address=address, complaint_type=complaint_type, threshold=threshold)
     return {"Total no. of complaints registered": total_complaints}
+
+
+def add_street(address, longitude, latitude, date_of_clearing, clearing_status):
+    snow_buddy_db.streets.insert_one({"address":address, "longitude":longitude, "latitude":latitude, "date_of_clearing":date_of_clearing, "clearing_status":clearing_status})
+    return {"Added address": ""}
+
+def get_new_route():
+    streets = snow_buddy_db.streets.find({"clearing_status": "False"})
+    streets = list(streets)
+
+    street_locations = []
+
+    for street in streets:
+        street_locations.append(street["longitude"] + " " + street["latitude"])
+
+    result = gmaps.directions(street_locations[0], street_locations[0], mode="driving", waypoints=street_locations[1:])
+
+    steps = ""
+
+    steps = []
+
+    for i, leg in enumerate(result[0]["legs"]):
+        steps = steps + "Stop: " + str(i) + " " + str(leg["start_location"])\
+                +" to " + " " + str(leg["end_location"])\
+                + " distance: " + str(leg["distance"]["value"])\
+                + " traveling Time: " +  str(leg["duration"]["value"]) + \
+                "\n"
+
+    print(steps)
+
+
+    return steps
+
